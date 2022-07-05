002963854df46b0ef99f80d82fd9824634934b3a
PrincessMaggy <edoziemagdalene@gmail.com>
2022-07-05 15:29:06 -0700

    ..

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is + or -
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+	i = 98;
+
+        positive_or_negative(i);
+
+        return (0);
+}
